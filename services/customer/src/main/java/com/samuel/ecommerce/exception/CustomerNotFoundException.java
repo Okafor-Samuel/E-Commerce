2bff77dfc72988386c63bf9efae34e6107874a4b
Okafor-samuel <okaforsamuel1000@gmail.com>
2024-07-18 13:24:43 +0100

    exception handler

---
+++ services/customer/src/main/java/com/samuel/ecommerce/exception/CustomerNotFoundException.java
@@ -0,0 +1,10 @@
+package com.samuel.ecommerce.exception;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class CustomerNotFoundException extends RuntimeException {
+    private final  String msg;
+}
