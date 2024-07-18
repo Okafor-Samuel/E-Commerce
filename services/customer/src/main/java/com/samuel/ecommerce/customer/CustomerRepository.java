11023a4f8bdabda043d2551d9d3945bee4ab0b8c
Okafor-samuel <okaforsamuel1000@gmail.com>
2024-07-18 13:26:06 +0100

    customer implementation

---
+++ services/customer/src/main/java/com/samuel/ecommerce/customer/CustomerRepository.java
@@ -0,0 +1,6 @@
+package com.samuel.ecommerce.customer;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface CustomerRepository extends MongoRepository<Customer, String> {
+}
