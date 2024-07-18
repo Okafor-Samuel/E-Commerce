8e5c629b40bf6348e5069b63dfeccb540f814098
Okafor-samuel <okaforsamuel1000@gmail.com>
2024-07-18 13:25:59 +0100

    customer implementation

---
+++ services/customer/src/main/java/com/samuel/ecommerce/customer/CustomerCreationRequest.java
@@ -0,0 +1,17 @@
+package com.samuel.ecommerce.customer;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+
+public record CustomerCreationRequest(
+        String id,
+        @NotNull(message = "Customer first name is required")
+        String firstName,
+        @NotNull(message = "Customer last name is required")
+        String lastName,
+        @NotNull(message = "Customer email is required")
+        @Email(message = "Customer email is not a valid email address")
+        String email,
+        Address address
+) {
+}
