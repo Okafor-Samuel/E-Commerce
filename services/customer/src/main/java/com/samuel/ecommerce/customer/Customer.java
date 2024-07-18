9073a29607d0b6c88be3f81ea5af7f6fba1ea6c6
Okafor-samuel <okaforsamuel1000@gmail.com>
2024-07-18 13:25:51 +0100

    customer implementation

--- services/customer/src/main/java/com/samuel/ecommerce/customer/Customer.java
+++ services/customer/src/main/java/com/samuel/ecommerce/customer/Customer.java
@@ -1,4 +1,20 @@
 package com.samuel.ecommerce.customer;
 
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Document
 public class Customer {
+    @Id
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Address address;
 }
