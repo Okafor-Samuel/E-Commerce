890f3f4380ea74217555cf0818a3910d38a0938e
Okafor-samuel <okaforsamuel1000@gmail.com>
2024-07-19 03:31:55 +0100

    product implementation

---
+++ services/product/src/main/java/com/samuel/ecommerce/product/Product.java
@@ -0,0 +1,26 @@
+package com.samuel.ecommerce.product;
+
+import com.samuel.ecommerce.category.Category;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String name;
+    private String description;
+    private double availableQuantity;
+    private BigDecimal price;
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+}
