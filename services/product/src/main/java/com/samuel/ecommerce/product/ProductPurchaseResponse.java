db96df300b140765a665f36d2c216a46ec535e40
Okafor-samuel <okaforsamuel1000@gmail.com>
2024-07-19 03:46:17 +0100

    product implementation

---
+++ services/product/src/main/java/com/samuel/ecommerce/product/ProductPurchaseResponse.java
@@ -0,0 +1,12 @@
+package com.samuel.ecommerce.product;
+
+import java.math.BigDecimal;
+
+public record ProductPurchaseResponse(
+        Integer productId,
+        String name,
+        String description,
+        BigDecimal price,
+        double quantity
+) {
+}
