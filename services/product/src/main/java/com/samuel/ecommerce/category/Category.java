9ac481093cf109143c15ac661c2b6f1e12fd9d7b
Okafor-samuel <okaforsamuel1000@gmail.com>
2024-07-19 03:31:43 +0100

    product implementation

---
+++ services/product/src/main/java/com/samuel/ecommerce/category/Category.java
@@ -0,0 +1,23 @@
+package com.samuel.ecommerce.category;
+
+import com.samuel.ecommerce.product.Product;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String name;
+    private String description;
+    @OneToMany(mappedBy = "category", cascade = CascadeType.REMOVE)
+    private List<Product> products;
+}
