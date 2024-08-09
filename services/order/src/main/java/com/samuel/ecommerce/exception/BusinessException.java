c7bec1ff425a5293373f96e0a0bf14121efd1dd0
Okafor-samuel <okaforsamuel1000@gmail.com>
2024-08-09 18:03:02 +0100

    order

---
+++ services/order/src/main/java/com/samuel/ecommerce/exception/BusinessException.java
@@ -0,0 +1,10 @@
+package com.samuel.ecommerce.exception;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class BusinessException extends RuntimeException {
+    private final String msg;
+}
