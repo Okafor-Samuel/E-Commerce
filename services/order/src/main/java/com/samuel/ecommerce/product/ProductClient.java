9b4635dbc0ba36eaf6c895a74c0d1cb2758b9cf4
Okafor-samuel <okaforsamuel1000@gmail.com>
2024-08-10 02:12:35 +0100

    order

---
+++ services/order/src/main/java/com/samuel/ecommerce/product/ProductClient.java
@@ -0,0 +1,43 @@
+package com.samuel.ecommerce.product;
+
+
+import com.samuel.ecommerce.exception.BusinessException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@Service
+@RequiredArgsConstructor
+public class ProductClient {
+    @Value("${application.config.product-url}")
+    private  String productUrl;
+
+    private final RestTemplate restTemplate;
+
+    public List<PurchaseResponse> purchasedProducts(List<PurchaseRequest> requestBody){
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(CONTENT_TYPE, APPLICATION_JSON_VALUE);
+
+        HttpEntity<List<PurchaseRequest>> requestEntity = new HttpEntity<>(requestBody, headers);
+        ParameterizedTypeReference<List<PurchaseResponse>> responseType = new ParameterizedTypeReference<>() {};
+
+        ResponseEntity<List<PurchaseResponse>> responseEntity = restTemplate.exchange(
+                productUrl + "/purchase",
+                HttpMethod.POST,
+                requestEntity,
+                responseType
+        );
+        if(responseEntity.getStatusCode().isError()){
+            throw new BusinessException("An error occurred while processing the product purchase: " + responseEntity.getStatusCode());
+        }
+        return responseEntity.getBody();
+    }
+}
