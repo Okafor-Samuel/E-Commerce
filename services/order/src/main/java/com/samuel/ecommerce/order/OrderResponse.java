aa897a5bc7a2fe532256ed701975e0cffa7e48af
Okafor-samuel <okaforsamuel1000@gmail.com>
2024-08-10 02:15:22 +0100

    order

---
+++ services/order/src/main/java/com/samuel/ecommerce/order/OrderResponse.java
@@ -0,0 +1,12 @@
+package com.samuel.ecommerce.order;
+
+import java.math.BigDecimal;
+
+public record OrderResponse(
+        Integer id,
+        String reference,
+        BigDecimal amount,
+        PaymentMethod paymentMethod,
+        String customerId
+) {
+}
