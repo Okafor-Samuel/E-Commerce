73cc1e910bbe380ac561d91018696cbf824482f8
Okafor-samuel <okaforsamuel1000@gmail.com>
2024-08-10 02:12:30 +0100

    order

---
+++ services/order/src/main/java/com/samuel/ecommerce/product/PurchaseRequest.java
@@ -0,0 +1,13 @@
+package com.samuel.ecommerce.product;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+public record PurchaseRequest(
+        @NotNull(message = "Product is mandatory")
+        Integer productId,
+
+        @Positive(message = "Quantity is mandatory")
+        double quantity
+) {
+}
