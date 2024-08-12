99dce702affe4e90ced1dd59a79df7e9c0fe8411
Okafor-samuel <okaforsamuel1000@gmail.com>
2024-08-12 04:42:28 +0100

    order

---
+++ services/order/src/main/java/com/samuel/ecommerce/orderLine/OrderLine.java
@@ -0,0 +1,25 @@
+package com.samuel.ecommerce.orderLine;
+
+import com.samuel.ecommerce.order.Order;
+import jakarta.persistence.*;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+public class OrderLine {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    private Integer productId;
+
+    private double quantity;
+}
