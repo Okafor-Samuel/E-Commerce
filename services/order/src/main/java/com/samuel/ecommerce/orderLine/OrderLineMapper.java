c90b2177c3d1b0400c032cced3c5a511ceeaf93c
Okafor-samuel <okaforsamuel1000@gmail.com>
2024-08-24 02:12:19 +0100

    order

---
+++ services/order/src/main/java/com/samuel/ecommerce/orderLine/OrderLineMapper.java
@@ -0,0 +1,25 @@
+package com.samuel.ecommerce.orderLine;
+
+import com.samuel.ecommerce.order.Order;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderLineMapper {
+    public OrderLine toOrderLine(OrderLineRequest orderLineRequest) {
+        return OrderLine.builder()
+                .id(orderLineRequest.id())
+                .quantity(orderLineRequest.quantity())
+                .order(Order.builder()
+                        .id(orderLineRequest.orderId())
+                        .build())
+                .productId(orderLineRequest.productId())
+                .build();
+    }
+
+    public OrderLineResponse toOrderLineResponse(OrderLine orderLine) {
+        return new OrderLineResponse(
+                orderLine.getId(),
+                orderLine.getQuantity()
+        );
+    }
+}
