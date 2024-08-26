417599f21064fbdbf105073219aabba107c28ab7
Okafor-samuel <okaforsamuel1000@gmail.com>
2024-08-26 06:48:19 +0100

    order

---
+++ services/order/src/main/java/com/samuel/ecommerce/order/Order.java
@@ -0,0 +1,45 @@
+package com.samuel.ecommerce.order;
+
+import com.samuel.ecommerce.orderLine.OrderLine;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "customer_order")
+public class Order {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String reference;
+
+    private BigDecimal totalAmount;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentMethod paymentMethod;
+
+    private String customerId;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderLine> orderLines;
+
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(insertable = false, nullable = false)
+    private LocalDateTime lastModifiedDate;
+}
