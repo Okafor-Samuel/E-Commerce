08667ea6015c644a89ae4868926f867a77ed7f70
Okafor-samuel <okaforsamuel1000@gmail.com>
2024-08-27 07:23:40 +0100

    order

---
+++ services/order/src/main/java/com/samuel/ecommerce/order/OrderController.java
@@ -0,0 +1,30 @@
+package com.samuel.ecommerce.order;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/order")
+@RequiredArgsConstructor
+public class OrderController {
+    private final OrderService orderService;
+
+    @PostMapping
+    public ResponseEntity<Integer> createOrder(@RequestBody @Valid OrderCreationRequest orderCreationRequest){
+        return ResponseEntity.ok(orderService.createOrder(orderCreationRequest));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<OrderResponse>> findAll(){
+        return ResponseEntity.ok(orderService.findAll());
+    }
+
+    @GetMapping("/{order-id}")
+    public ResponseEntity<OrderResponse> findById(@PathVariable("order-id") Integer orderId){
+        return ResponseEntity.ok(orderService.findById(orderId));
+    }
+}
