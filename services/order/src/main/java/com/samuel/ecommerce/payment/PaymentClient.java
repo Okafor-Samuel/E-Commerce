324d94c975f7ad6140b516b72a1dbc5cd1583485
Okafor-samuel <okaforsamuel1000@gmail.com>
2024-08-28 01:24:28 +0100

    payment

---
+++ services/order/src/main/java/com/samuel/ecommerce/payment/PaymentClient.java
@@ -0,0 +1,14 @@
+package com.samuel.ecommerce.payment;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(
+        name = "product-service",
+        url = "${application.config.payment-url}"
+)
+public interface PaymentClient {
+    @PostMapping
+    Integer requestOrderPayment(@RequestBody PaymentRequest request);
+}
