217582f03c143f4f34ebffba2a38fb4c85f4bb2f
Okafor-samuel <okaforsamuel1000@gmail.com>
2024-08-30 06:07:49 +0100

    payment

---
+++ services/payment/src/main/java/com/samuel/ecommerce/payment/payment/PaymentMapper.java
@@ -0,0 +1,15 @@
+package com.samuel.ecommerce.payment.payment;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentMapper {
+    public Payment toPayment(PaymentRequest paymentRequest) {
+        return Payment.builder()
+                .id(paymentRequest.id())
+                .orderId(paymentRequest.orderId())
+                .paymentMethod(paymentRequest.paymentMethod())
+                .amount(paymentRequest.amount())
+                .build();
+    }
+}
