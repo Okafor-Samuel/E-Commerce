f5814cf415e85646861dea54de7d4e5023ee9809
Okafor-samuel <okaforsamuel1000@gmail.com>
2024-09-11 07:06:00 +0100

    payment

---
+++ services/payment/src/main/java/com/samuel/ecommerce/payment/payment/PaymentRequest.java
@@ -0,0 +1,14 @@
+package com.samuel.ecommerce.payment.payment;
+
+import java.math.BigDecimal;
+
+public record PaymentRequest(
+        Integer id,
+        BigDecimal amount,
+        PaymentMethod paymentMethod,
+        Integer orderId,
+        String orderReference,
+        Customer customer
+
+) {
+}
