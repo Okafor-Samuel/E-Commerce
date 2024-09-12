c69ec43a94373024b0276441d1964e6ee9a1bb03
Okafor-samuel <okaforsamuel1000@gmail.com>
2024-09-12 07:53:28 +0100

    payment

---
+++ services/payment/src/main/java/com/samuel/ecommerce/payment/notification/PaymentNotificationRequest.java
@@ -0,0 +1,16 @@
+package com.samuel.ecommerce.payment.notification;
+
+import com.samuel.ecommerce.payment.payment.PaymentMethod;
+
+import java.math.BigDecimal;
+
+public record PaymentNotificationRequest(
+        String orderReference,
+        BigDecimal amount,
+        PaymentMethod paymentMethod,
+        String customerFirstName,
+        String customerLastName,
+        String customerEmail
+
+) {
+}
