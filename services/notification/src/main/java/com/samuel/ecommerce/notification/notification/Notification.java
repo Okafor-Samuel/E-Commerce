593a9364267521947d8661e6cd4a004270c2331f
Okafor-samuel <okaforsamuel1000@gmail.com>
2024-09-21 16:40:41 +0100

    notification

---
+++ services/notification/src/main/java/com/samuel/ecommerce/notification/notification/Notification.java
@@ -0,0 +1,25 @@
+package com.samuel.ecommerce.notification.notification;
+
+import com.samuel.ecommerce.notification.kafka.order.OrderConfirmation;
+import com.samuel.ecommerce.notification.kafka.payment.PaymentConfirmation;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Document
+public class Notification {
+    @Id
+    private String id;
+    private NotificationType notificationType;
+    private LocalDateTime notificationDate;
+    private OrderConfirmation orderConfirmation;
+    private PaymentConfirmation paymentConfirmation;
+
+}
