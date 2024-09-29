0f367e42c4aa965ab87a52134cfe59fd0fd44930
Okafor-samuel <okaforsamuel1000@gmail.com>
2024-09-29 07:28:40 +0100

    payment

--- services/order/src/main/java/com/samuel/ecommerce/PaymentClient.java
+++ services/order/src/main/java/com/samuel/ecommerce/PaymentClient.java
@@ -1,4 +1,4 @@
-package com.samuel.ecommerce.payment;
+package com.samuel.ecommerce;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
