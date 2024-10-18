e17f5703a839cb531ce8afad8ef2ed55ae2a9a0e
Okafor-samuel <okaforsamuel1000@gmail.com>
2024-10-18 17:54:53 +0100

    payment

--- services/order/src/main/java/com/samuel/ecommerce/customer/CustomerClient.java
+++ services/order/src/main/java/com/samuel/ecommerce/customer/CustomerClient.java
@@ -6,10 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Optional;
 
-@FeignClient(
-        name = "customer-service",
-        url = "${application.config.customer-url}"
-)
+@FeignClient(name = "customer-service", url = "${application.config.customer-url}")
 public interface CustomerClient {
     @GetMapping("/{customer-id}")
     Optional<CustomerResponse> findCustomerById(@PathVariable("customer-id") String customerId);
